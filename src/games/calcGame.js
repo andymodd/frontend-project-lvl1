d9cf77c7beb280250aafa461a5b7ba2296aa9c5f
Andrey Glushko <andrew.modd@gmail.com>
2021-12-29 15:04:44 +0500

    fix calculation game

--- src/games/calcGame.js
+++ src/games/calcGame.js
@@ -1,38 +1,32 @@
-import {
-  numberOfGames, getRandomNumber, checkAnswer, askQuestionAndGetAnswer, greetUser,
-} from '../index.js';
+import { startGame, getRandomNumber, numberOfGames } from '../index.js';
 
-const arithmeticOperators = ['+', '-', '*'];
-
-const startGame = () => {
-  const userName = greetUser();
-  console.log('What is the result of the expression?');
-  for (let counter = 0; counter < numberOfGames; counter += 1) {
-    const firstOperand = getRandomNumber(100);
-    const secondOperand = getRandomNumber(100);
-    const operator = arithmeticOperators[getRandomNumber(2)];
-    const question = `Question: ${firstOperand} ${operator} ${secondOperand} `;
-    const userAnswer = Number(askQuestionAndGetAnswer(question));
-    let rightAnswer;
-    switch (operator) {
-      case '+':
-        rightAnswer = firstOperand + secondOperand;
-        break;
-      case '-':
-        rightAnswer = firstOperand - secondOperand;
-        break;
-      case '*':
-        rightAnswer = firstOperand * secondOperand;
-        break;
-      default:
+const startCalcGame = () => {
+  const ruleOfGame = 'What is the result of the expression?';
+  const arithmeticOperators = ['+', '-', '*'];
+  const questions = [];
+  const rightAnswers = [];
+  const createQuestions = () => {
+    for (let i = 0; i < numberOfGames; i += 1) {
+      const firstOperand = getRandomNumber(100);
+      const secondOperand = getRandomNumber(100);
+      const operator = arithmeticOperators[getRandomNumber(2)];
+      questions.push(`${firstOperand} ${operator} ${secondOperand}`);
+      switch (operator) {
+        case '+':
+          rightAnswers.push(`${firstOperand + secondOperand}`);
+          break;
+        case '-':
+          rightAnswers.push(`${firstOperand - secondOperand}`);
+          break;
+        case '*':
+          rightAnswers.push(`${firstOperand * secondOperand}`);
+          break;
+        default:
+      }
     }
-    if (!checkAnswer(userAnswer, rightAnswer)) {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}.\n Let's try again, ${userName}!`);
-      return;
-    }
-    console.log('Correct!');
-  }
-  console.log(`Congratulations, ${userName}!`);
+  };
+  createQuestions();
+  startGame(ruleOfGame, questions, rightAnswers, numberOfGames);
 };
 
-export default startGame;
+export default startCalcGame;
