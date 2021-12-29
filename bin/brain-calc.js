a4ffcce16a4ab7631cd4adedd2d120025619f35c
Andrey Glushko <andrew.modd@gmail.com>
2021-12-29 15:05:13 +0500

    fix bin files of two games

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import startGame from '../src/games/calcGame.js';
+import startCalcGame from '../src/games/calcGame.js';
 
-startGame();
+startCalcGame();
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import startGame from '../src/games/isEvenGame.js';
+import startIsEvenGame from '../src/games/isEvenGame.js';
 
-startGame();
+startIsEvenGame();
