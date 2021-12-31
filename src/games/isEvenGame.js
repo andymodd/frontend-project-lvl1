a5da8648d5a9a7ca353ded5986751e31fc6063db
Andrey Glushko <andrew.modd@gmail.com>
2021-12-31 15:15:50 +0500

    fix side effects of function createQuestions

--- src/games/isEvenGame.js
+++ src/games/isEvenGame.js
@@ -3,16 +3,25 @@ import { startGame, getRandomNumber, numberOfGames } from '../index.js';
 const startIsEvenGame = () => {
   const ruleOfGame = 'Answer "yes" if the number is even, otherwise answer "no".';
   const isEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
-  const questions = [];
-  const rightAnswers = [];
   const createQuestions = () => {
+    const questions = [];
     for (let i = 0; i < numberOfGames; i += 1) {
       const question = getRandomNumber(100);
       questions.push(question);
-      rightAnswers.push(isEven(question));
     }
+    return questions;
   };
-  createQuestions();
+  const createRightAnswers = (questionsOfGame) => {
+    const rightAnswers = [];
+    for (let i = 0; i < questionsOfGame.length; i += 1) {
+      const rightAnswer = isEven(questionsOfGame[i]);
+      rightAnswers.push(rightAnswer);
+    }
+    return rightAnswers;
+  };
+  const questions = createQuestions();
+  const rightAnswers = createRightAnswers(questions);
+  createQuestions(questions);
   startGame(ruleOfGame, questions, rightAnswers, numberOfGames);
 };
 
